b7179e3f6cb71fe5920221b80dc248b57c090cd2
ajs <140010746+aboodJS@users.noreply.github.com>
2026-03-26 17:26:39 +0300

    added simple component

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { javascript } from '@codemirror/lang-javascript';
+import CodeMirror from 'vue-codemirror6';
+import { ref } from 'vue';
+
+const lang = javascript()
+
+const value = ref("console.log('hello world')")
+</script>
+
+<template>
+   <code-mirror v-model="value" :lang="lang" />
+</template>
+
+<style scoped></style>
