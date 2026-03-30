2c900c84bfa28b02b6dabb17e7f2a28c9bed477b
ajs <140010746+aboodJS@users.noreply.github.com>
2026-03-30 22:13:53 +0300

    added a name field

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,12 +4,19 @@ import CodeMirror from 'vue-codemirror6';
 import { ref } from 'vue';
 
 const lang = javascript()
-
 const value = ref("console.log('hello world')")
+const snippetName = ref("")
+
+async function sendData() {
+   console.log(value.value)
+}
+
 </script>
 
 <template>
+   <input type="text" placeholder="enter snippet name" v-model="snippetName">
    <code-mirror v-model="value" :lang="lang" />
+   <button @click="sendData">Submit</button>
 </template>
 
 <style scoped></style>
